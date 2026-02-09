bdb612631cab5ffc0d85f3d130eecc4c988d61f1
Pratham Mishra <pratham19092006@gmail.com>
2026-02-09 07:52:58 +0530

    Introduction to Javascript

---
+++ lecture39/index.js
@@ -0,0 +1,12 @@
+// it is a scripting language(light-wieght programming language) used to add behaviour, functionalities, and brain to the web pages. 
+// It is a client-side language that runs in the browser and allows developers to create interactive and dynamic web applications. JavaScript can be used for various purposes, such as form validation, creating animations, handling events, making API calls, and manipulating the DOM (Document Object Model) to update the content of a web page without reloading it. It is an essential part of modern web development and is supported by all major browsers.
+
+// CPP program + javascript = nodejs
+
+// ECMA: European Computer Manufacturers Association, it is a standardization body that develops and maintains standards for various technologies, including programming languages. ECMA is responsible for standardizing JavaScript, which is officially known as ECMAScript. The ECMAScript specification defines the syntax, semantics, and behavior of the JavaScript language, ensuring that it remains consistent across different implementations and platforms. The latest version of ECMAScript is ECMAScript 2024 (ES2024), which includes new features and improvements to the language.
+
+// Node.js is a runtime environment that allows developers to run JavaScript code outside of a web browser. It is built on the V8 JavaScript engine, which is the same engine used by Google Chrome. Node.js provides an event-driven, non-blocking I/O model that makes it efficient and suitable for building scalable network applications. With Node.js, developers can use JavaScript to create server-side applications, command-line tools, and even desktop applications. It has a vast ecosystem of libraries and frameworks that make it easier to develop various types of applications using JavaScript.
+
+// =============================================
+
+console.log("my name is pratham learning with babbar")
