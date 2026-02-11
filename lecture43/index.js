0472b5b543a955ac50fa94e8219f96d85b3cea18
Pratham Mishra <pratham19092006@gmail.com>
2026-02-11 07:46:59 +0530

    Functions

---
+++ lecture43/index.js
@@ -0,0 +1,72 @@
+// function sayMyName(){
+//     console.log("pratham mishra");
+// }
+
+// sayMyName();
+
+
+// function printCounting(){
+//     for(let i = 1; i<=100; i++){
+//         process.stdout.write(i + " ");
+//     }
+// }
+
+// printCounting();
+
+
+// function printNumber(num){
+//     console.log(num);
+// }
+
+// printNumber(10);
+
+
+// function getAvg(num1, num2){
+//     let avg = (num1 + num2) / 2;
+//     console.log(avg);
+// }
+// getAvg(10, 20);
+
+
+// function getSum(num1, num2, num3){
+//     let sum = num1 + num2 + num3;
+//     return sum;
+// }
+// let result = getSum(1,2,3);
+// console.log(result);
+
+// function getMyName(firstName, lastName){
+//     let fullName = firstName + " " + lastName;
+//     return fullName;
+//     // unrreachable code
+//     // console.log("this will not be printed");
+// }
+// let fullName = getMyName("pratham", "mishra");
+// console.log(fullName);
+// fullName = getMyName(7, "doe");
+// console.log(fullName);
+
+// function multiplication(num1, num2){
+//     return num1 * num2;
+// }
+// console.log(multiplication(5, 10));
+
+
+// other way to write function
+// let myFunc = function(num1, num2){
+//     return num1 * num2;
+// }
+// let ans = myFunc(5, 10);
+// console.log(ans);
+
+
+// const getSum =  function(num1, num2){
+//     return num1 + num2;
+// }
+// console.log(getSum(10, 20));
+
+// arrow function
+// const squareNumber = (num1) => {
+//     return (num1**2);
+// }
+// console.log(squareNumber(9));
